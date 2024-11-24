af637ad240e677235226794decdcd1b51aab8f17
Aniko Sallai <aniko.sallai@gmail.com>
2024-11-24 21:45:42 +0100

    Refactored based on review comments

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -4,7 +4,7 @@ test.describe('Input fields', async () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/')
 		await page.getByText('Pet Types').click()
-		await expect(page.getByRole('heading', { name: 'Pet Types' })).toBeVisible()
+		await expect(page.getByRole('heading')).toHaveText('Pet Types')
 	})
 
 	test('Update pet type', async ({ page }) => {
@@ -16,26 +16,23 @@ test.describe('Input fields', async () => {
 		const firstEditButton = page.getByRole('button', { name: 'Edit' }).first()
 		const firstPetTypeInput = page.locator('[name="pettype_name"]').first()
 		// Locators - Edit Pet Types page
-		const editPetTypeHeader = page.getByRole('heading', { name: 'Edit Pet Type' })
 		const petTypeInput = page.locator('#name')
 		const petTypeUpdateButton = page.getByRole('button', { name: 'Update' })
 
 		// Test steps
 		await firstEditButton.click()
-		await expect(editPetTypeHeader).toBeVisible()
+		await expect(page.getByRole('heading')).toHaveText('Edit Pet Type')
 
 		await petTypeInput.click()
 		await petTypeInput.fill(newPetType)
 		await petTypeUpdateButton.click()
-		let inputValue = await firstPetTypeInput.inputValue()
-		expect(inputValue).toBe(newPetType)
+		await expect(firstPetTypeInput).toHaveValue(newPetType)
 
 		await firstEditButton.click()
 		await petTypeInput.click()
 		await petTypeInput.fill(originalPetType)
 		await petTypeUpdateButton.click()
-		inputValue = await firstPetTypeInput.inputValue()
-		expect(inputValue).toBe(originalPetType)
+		await expect(firstPetTypeInput).toHaveValue(originalPetType)
 	})
 
 	test('Cancel pet type update', async ({ page }) => {
@@ -43,46 +40,33 @@ test.describe('Input fields', async () => {
 		const originalPetType = 'dog'
 		const newPetType = 'moose'
 
-		// Locators - Pet Types table page
-		const secondEditButton = page.getByRole('button', { name: 'Edit' }).nth(1)
-		const secondPetTypeInput = page.locator('[name="pettype_name"]').nth(1)
-		// Locators - Edit Pet Type page
+		// Locator - Edit Pet Type page
 		const petTypeInput = page.locator('#name')
-		const petTypeCancelButton = page.getByRole('button', { name: 'Cancel' })
 
 		// Test steps
-		await secondEditButton.click()
+		await page.getByRole('button', { name: 'Edit' }).nth(1).click()
 		await petTypeInput.click()
 		await petTypeInput.fill(newPetType)
-		let inputValue = await petTypeInput.inputValue()
-		expect(inputValue).toBe(newPetType)
+		await expect(petTypeInput).toHaveValue(newPetType)
 
-		await petTypeCancelButton.click()
-		inputValue = await secondPetTypeInput.inputValue()
-		expect(inputValue).toBe(originalPetType)
+		await page.getByRole('button', { name: 'Cancel' }).click()
+		await expect(page.locator('[name="pettype_name"]').nth(1)).toHaveValue(originalPetType)
 	})
 
 	test('Pet type name is required validation', async ({ page }) => {
-		// Locators - Pet Types table page
-		const tableHeader = page.getByRole('heading', { name: 'Pet Types' })
-		const thirdEditButton = page.getByRole('button', { name: 'Edit' }).nth(2)
-		// Locators - Edit Pet Type page
-		const editPetTypeHeader = page.getByRole('heading', { name: 'Edit Pet Type' })
+		// Locator - Edit Pet Type page
 		const petTypeInput = page.locator('#name')
-		const petTypeUpdateButton = page.getByRole('button', { name: 'Update' })
-		const petTypeCancelButton = page.getByRole('button', { name: 'Cancel' })
-		const blockMessage = page.locator('.help-block')
 
 		// Test steps
-		await thirdEditButton.click()
+		await page.getByRole('button', { name: 'Edit' }).nth(2).click()
 		await petTypeInput.click()
 		await petTypeInput.clear()
-		await expect(blockMessage).toHaveText('Name is required')
+		await expect(page.locator('.help-block')).toHaveText('Name is required')
 
-		await petTypeUpdateButton.click()
-		await expect(editPetTypeHeader).toBeVisible()
+		await page.getByRole('button', { name: 'Update' }).click()
+		await expect(page.getByRole('heading')).toHaveText('Edit Pet Type')
 
-		await petTypeCancelButton.click()
-		await expect(tableHeader).toBeVisible()
+		await page.getByRole('button', { name: 'Cancel' }).click()
+		await expect(page.getByRole('heading')).toHaveText('Pet Types')
 	})
 })
